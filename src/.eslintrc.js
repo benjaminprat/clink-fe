4e14c7f4217e546b2cd519cd5b562fc3e29b9e2a
Ben Prat <bvprat@gmail.com>
2021-04-23 15:29:14 -0600

    Commit

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -7,5 +7,6 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint",
   },
-  rules: {},
+  rules: {"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+  "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",},
 };
