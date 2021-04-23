87e2bf04815fb274e33fdbfbbbe3ca1c7bab3b75
Ben Prat <bvprat@gmail.com>
2021-04-23 15:25:31 -0600

    Updat original functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
-  <span><h1>Welcome to the hit parade</h1></span>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/signup">Signup</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/wines">Show All Wines</router-link> |
+      <router-link to="/wines/new">Add A New Wine</router-link> |
+      <router-link to="/card"> New Card</router-link> |
+      <router-link to="/logout">Logout</router-link> |
+    </div>
+    <router-view />
+  </div>
 </template>
 
-<script>
-export default {};
-</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
- <style></style>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
