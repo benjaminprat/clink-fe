bebf78126eb5cc5e03a3f843443427d23b5d8f51
Ben Prat <bvprat@gmail.com>
2021-04-23 15:20:16 -0600

    Update index.js with original

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,59 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import WinesIndex from "../views/WinesIndex.vue";
+import WinesCreate from "../views/WinesCreate.vue";
+import Logout from "../views/Logout.vue";
+import Login from "../views/Login.vue";
+import Card from "../views/Card.vue";
+import Signup from "../views/Signup.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/wines/new",
+    name: "WinesCreate",
+    component: WinesCreate,
+  },
+  {
+    path: "/wines",
+    name: "WinesIndex",
+    component: WinesIndex,
+  },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/signup", name: "Signup", component: Signup },
+
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
+  },
+  {
+    path: "/cards/:id",
+    name: "Card",
+    component: Card,
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
 
-export default router
+export default router;
