cc46995263cdbc081a45c4dae4b0dbfa92f24c4b
Ben Prat <bvprat@gmail.com>
2021-04-23 15:23:58 -0600

    Add Wine Index Functionality

---
+++ src/views/WinesIndex.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wines-index">
+    <h1>{{ message }}</h1>
+    <h1>
+      <div v-for="wine in wines" v-bind:key="wine.id">
+        {{ wine.producer_wine }}
+      </div>
+    </h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Here are all your wines",
+      wines: [],
+      style: this.style,
+      name: this.name,
+      producer_wine: this.producer_wine,
+      grape: this.grape,
+      region: this.region,
+      vintage: this.vintage,
+      country: this.country,
+    };
+  },
+  created: function () {
+    this.winesIndex();
+  },
+  methods: {
+    winesIndex: function () {
+      console.log("all wines");
+      axios.get("api/wines").then((response) => {
+        this.wines = response.data;
+        console.log(response.data);
+      });
+    },
+  },
+};
+</script>
