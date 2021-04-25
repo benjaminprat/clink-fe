d795af024e2a364c21d03de6e06aa37c7f42fce8
Ben Prat <bvprat@gmail.com>
2021-04-24 18:41:45 -0600

    Move router links to appropriate

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
 <template>
   <div id="app">
-  <div class="bs-example">
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <a href="/" class="navbar-brand">Clink! Clink!</a>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav">
-                <a href="/" class="nav-item nav-link active">Home</a>
-                <a href="/about" class="nav-item nav-link">About</a>
-                <a href="/wines" class="nav-item nav-link">Show All Wines</a>
-                <a href="/wines/new" class="nav-item nav-link">Add A New Wine</a>
-                <a href="/card" class="nav-item nav-link">New Entry</a>
-                <a href="/signup" class="nav-item nav-link">Signup</a>
-                <a href="/login" class="nav-item nav-link">Login</a>
-                <a href="/logout" class="nav-item nav-link">Logout</a>
-            </div>
-        </div>
-    </nav>
-</div>
-    <router-view />
+     <router-view />
   </div>
 </template>
 
 <style>
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Roboto', sans-serif;
+  height: 100vh;
   text-align: center;
-  color: #2c3e50;
+  background: #11101A;
+  /* color: #2c3e50; */
+  color:#fffffe;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+
 </style>
 <script>
   export default {
