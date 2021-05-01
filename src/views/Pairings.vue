02e0555e664f974809567b1fb6fa046b94ba8938
Ben Prat <bvprat@gmail.com>
2021-05-01 17:59:21 -0600

    Filter recipes

--- src/views/Pairings.vue
+++ src/views/Pairings.vue
@@ -1,14 +1,21 @@
 <template >
-<div class="pairings" >
-  <h1>{{message}}</h1>
-  <div v-for="pairing in pairings" v-bind:key="pairing.id">
+<div class="pairings-index" >
+  <p>Search: <input type="text" v-model="searchTerm"></p>
+  <br>
+  <div class="row">
+  <div class="col-sm-4" v-bind:key="pairing.id" v-for="pairing in filterBy(pairings, searchTerm, 'ingredient')">
+    <div class="card">
+       <div class="card-body">
 <p>Pairing Id: {{pairing.id}}</p>
 
-<p>Ingredient name: {{pairing.ingredient}}</p>
-<p>Ingredient Category: {{pairing.category}}</p>
-<p>Ingredient Sub Category: {{pairing.sub_category}}</p>
-<p>Wine Style: {{pairing.wine_style}}</p>
-<p>Is perfect pairing? {{pairing.perfect_pairing}}</p>
+<p class="card-text">Ingredient name: {{pairing.ingredient}}</p>
+<p class="card-text">Ingredient Category: {{pairing.category}}</p>
+<p class="card-text">Ingredient Sub Category: {{pairing.sub_category}}</p>
+<p class="card-text">Wine Style: {{pairing.wine_style}}</p>
+<p class="card-text">Is perfect pairing? {{pairing.perfect_pairing}}</p>
+</div>
+</div>
+  </div>
   </div>
 
 </div>
@@ -24,17 +31,15 @@
 </style>
 
 <script>
-import Vue2Filters from "vue2-filters";
 import axios from 'axios';
+import Vue2Filters from "vue2-filters";
 export default {
   mixins: [Vue2Filters.mixin],
   data: function () {
     return {
       message: "Pair Your Shit",
-     pairings: [],
-      ingredient_id: this.ingredient_id,
-      classification_id: this.classification_id,
-      perfect_pairing: this.perfect_pairing
+      pairings: [],
+      searchTerm: ""
       }
     }, 
   created: function() {
